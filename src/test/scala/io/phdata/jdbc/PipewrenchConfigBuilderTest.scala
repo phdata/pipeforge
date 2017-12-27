f84a72078235c7f4224b216fd2ededc714ac8452
Troy Fokken <troy@phdata.io>
2017-12-27 12:31:42 -0600

    Removed the old pipewrench conf file in favor of metadata.yml
    Added CLI Command line args:
    - database-configuration (s) - Path to the source database configuration
    file
    - database-password (p) Required - The source database password
    - table-metadata (m) - Path to the metadata yml file which will be
    used the enhance the tables.yml output
    - output-path (o) - Output path for the file generated tables.yml
    file
    
    Removed password from source configuration in favor of cli argument

--- src/test/scala/io/phdata/jdbc/PipewrenchConfigBuilderTest.scala
+++ src/test/scala/io/phdata/jdbc/PipewrenchConfigBuilderTest.scala
@@ -7,7 +7,6 @@ import io.phdata.jdbc.util.YamlWrapper
 import org.scalatest.FunSuite
 
 class PipewrenchConfigBuilderTest extends FunSuite {
-  val initialConfDataPath = "src/test/resources/initialConfData.yml"
   val initialTableDataPath = "src/test/resources/initialTableData.yml"
 
   test("Create config") {
@@ -16,11 +15,8 @@ class PipewrenchConfigBuilderTest extends FunSuite {
             Set(Column("REGION_ID", JDBCType.NUMERIC, false, 1, 0, -127)),
             Set(Column("REGION_NAME", JDBCType.VARCHAR, true, 2, 25, 0))))
 
-    val result =
-      PipewrenchConfigBuilder.buildPipewrenchConfig(
-        YamlWrapper.read(initialConfDataPath),
-        YamlWrapper.read(initialTableDataPath),
-        tables)
+    val result = Map("tables" ->
+      PipewrenchConfigBuilder.buildPipewrenchConfig(tables, YamlWrapper.read(initialTableDataPath)))
 
     // @TODO assert something
     assert(result != null)

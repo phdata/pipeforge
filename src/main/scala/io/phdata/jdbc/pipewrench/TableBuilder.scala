f77d39e0c60442677bccca0de8a4a956e712b256
Troy Fokken <troy@phdata.io>
2017-12-27 10:41:06 -0600

    Minor clean up

--- src/main/scala/io/phdata/jdbc/pipewrench/TableBuilder.scala
+++ src/main/scala/io/phdata/jdbc/pipewrench/TableBuilder.scala
@@ -7,35 +7,28 @@ import io.phdata.jdbc.domain.Table
 object TableBuilder {
   def buildTablesSection(tableMetadata: Set[Table],
                          initialData: Map[String, Object]) = {
-    val sorted = tableMetadata.toList.sortBy(_.name)
-    sorted.map(t => buildTable(t) ++ initialData)
+    tableMetadata.toList
+      .sortBy(_.name)
+      .map(t => buildTable(t) ++ initialData)
   }
 
   def buildTable(table: Table) = {
-    val allColumns = table.primaryKeys.toSeq ++ table.columns.toSeq
+    val allColumns = table.primaryKeys ++ table.columns
 
     Map(
       "id" -> table.name,
-      "source" ->
-        Map("name" -> table.name),
+      "source" -> Map("name" -> table.name),
       "split_by_column" -> getSplitByColumn(table),
-      "destination" ->
-        Map("name" -> table.name.toLowerCase),
+      "destination" -> Map("name" -> table.name.toLowerCase),
       "columns" -> ColumnBuilder.buildColumns(allColumns),
-      "primary_keys" -> table.primaryKeys.toList.map(_.name)
+      "primary_keys" -> table.primaryKeys.map(_.name)
     )
   }
 
   def getSplitByColumn(table: Table) = {
-    table.primaryKeys
-      .filter(x => x.dataType == JDBCType.BIGINT)
-      .headOption
-      .orElse(
-        table.primaryKeys.toList.headOption
-      )
-      .orElse(
-        table.columns.toList.headOption
-      )
+    table.primaryKeys.find(x => x.dataType == JDBCType.BIGINT)
+      .orElse(table.primaryKeys.headOption)
+      .orElse(table.columns.headOption)
       .get
       .name
   }

e4acb1219a2b170470cac20a6f7f7563b656a627
Troy Fokken <troy.a.fokken@gmail.com>
2018-03-07 20:49:47 -0600

    Cleaned up yaml formatters

--- rest-api/src/main/scala/io/phdata/pipeforge/rest/domain/YamlSupport.scala
+++ rest-api/src/main/scala/io/phdata/pipeforge/rest/domain/YamlSupport.scala
@@ -1,16 +1,27 @@
+/*
+ * Copyright 2018 phData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.phdata.pipeforge.rest.domain
 
-import io.phdata.pipewrench.domain.{ Column, Configuration, Table }
 import net.jcazevedo.moultingyaml.DefaultYamlProtocol
 import net.jcazevedo.moultingyaml._
 
 import scala.io.Source
 
 trait YamlSupport extends DefaultYamlProtocol {
-
-  implicit def columnYamlFormat        = yamlFormat5(Column)
-  implicit def tableYamlFormat         = yamlFormat7(Table)
-  implicit def configurationYamlFormat = yamlFormat10(Configuration)
 
   implicit def environmentYamlFormat = yamlFormat13(Environment)
 

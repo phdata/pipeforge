b195458d2cedbd986809180f1970a79a23b67e7b
Troy Fokken <troy@phdata.io>
2018-10-30 15:31:32 -0500

    Fixed if logic

--- common/src/main/scala/io/phdata/pipeforge/common/jdbc/DataType.scala
+++ common/src/main/scala/io/phdata/pipeforge/common/jdbc/DataType.scala
@@ -7,13 +7,13 @@ object DataType extends Enumeration {
   def mapDataType(column: Column): JDBCType = {
     val dataType = column.dataType
     if (dataType == JDBCType.NUMERIC) {
-      if (column.scale == 0) {
+      if (column.scale > 0) {
         JDBCType.DECIMAL
-      } else if (column.precision > 19) {
+      } else if (column.precision > 19 && column.scale == 0) {
         JDBCType.DECIMAL
-      } else if (column.precision >= 10 && column.precision <= 19) {
+      } else if (column.precision >= 10 && column.precision <= 19 && column.scale == 0) {
         JDBCType.BIGINT
-      } else if (column.scale == -127 && column.precision == 0) {
+      } else if (column.precision == 0 && column.scale == -127) {
         // Oracle datatypes can be defined simply as NUMBER with no precision or scale
         // In this case the value can be either 123 or 1.23 so keep these as Strings
         JDBCType.VARCHAR
